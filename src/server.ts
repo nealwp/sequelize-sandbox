cbfb8ad0268331b3fb29ab8a9ec63c68ced9fe64
nealwp <mrprestonneal@gmail.com>
2023-01-28 22:30:36 -0800

    add bodyparser middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import bodyParser, { json } from 'body-parser'
 import router from './routes'
 
 const server = express()
 
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({extended: true}))
 server.use(router)
 
 export default server
