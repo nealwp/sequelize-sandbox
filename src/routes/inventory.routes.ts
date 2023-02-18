3e5b9d13f2747186081731e412ccfa0321cd93e7
nealwp <mrprestonneal@gmail.com>
2023-02-18 15:41:28 -0800

    #6 add tests for inventory routes

--- src/routes/inventory.routes.ts
+++ src/routes/inventory.routes.ts
@@ -6,7 +6,10 @@ const router = Router()
 
 router.get('/', async (req, res, next) => {
     const allInventories = await inventory.findAll()
-    res.status(200).json(allInventories)
+    if(!allInventories.length){
+        return res.status(204).send()
+    }
+    return res.status(200).json(allInventories)
 })
 
 router.get('/:id', async (req, res, next) => {
@@ -16,7 +19,7 @@ router.get('/:id', async (req, res, next) => {
         res.status(200).json(foundInventory)
     } catch (error) {
         console.error(error)
-        res.status(404).send()
+        res.status(404).send(`inventory with id ${id} not found`)
     }
 })
 
@@ -27,7 +30,7 @@ router.post('/', async (req, res, next) => {
         res.status(201).json(createdInventory)
     } catch(error) {
         console.error(error)
-        res.status(500).send()
+        res.status(500).send(`error creating inventory for character ${payload.characterId}`)
     } 
 })
 
