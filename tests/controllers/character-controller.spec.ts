a7313a15e9ad3bdb0946fac786555b4991ab2c62
nealwp <mrprestonneal@gmail.com>
2023-02-18 19:04:25 -0800

    #7 add character controller spec file

---
+++ tests/controllers/character-controller.spec.ts
@@ -0,0 +1,17 @@
+describe('character controller', () => {
+    describe('character.create', () => {
+
+    })
+
+    describe('character.update', () => {
+
+    })
+
+    describe('character.findById', () => {
+
+    })
+
+    describe('character.findAll', () => {
+        
+    })
+})
